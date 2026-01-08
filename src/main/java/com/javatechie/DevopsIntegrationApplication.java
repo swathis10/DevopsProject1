302f251481cc50e253f2af47fd2418d4929e4ffe
swathis10 <60876070+swathis10@users.noreply.github.com>
2026-01-08 21:27:48 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to our DevOps Project !!";
+		return "Welcome to Microdegree DevOps workshop !!";
 	}
 
 	public static void main(String[] args) {
